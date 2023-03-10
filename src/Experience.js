66e6316e3bed7d234364f319b341791721e079cf
zvitez <zvitez@gmail.com>
2023-03-10 23:28:14 +0000

    Added an island model and adjusted workstation position

--- src/Experience.js
+++ src/Experience.js
@@ -1,9 +1,11 @@
 import React, { Suspense } from "react";
 import { WorkStation } from "./components/WorkStation/WorkStation";
+import { Model as Island } from "./components/Island/Scene";
 
 export function Experience() {
   return (
     <Suspense fallback={null}>
+      <Island />
       <WorkStation />
     </Suspense>
   );
