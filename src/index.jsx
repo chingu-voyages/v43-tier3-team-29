745a3d87f16045bd544716f12b172dba81cd9821
zvitez <zvitez@gmail.com>
2023-03-15 00:13:47 +0000

    Added various effects

--- src/index.jsx
+++ src/index.jsx
@@ -5,17 +5,18 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, BakeShadows } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { Experience } from "./Experience";
+import { Effects } from "./Effects";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [-15, 5, -10], fov: 75 }}>
       <color args={["#111111"]} attach="background" />
-      <Perf />
+      {/* <Perf /> */}
       <OrbitControls makeDefault target={[0, 0, 5]}/>
       <ambientLight intensity={0.02} />
-      <pointLight distance={16} castShadow shadow-bias={-0.0001} position={[-5, 4, 1]} />
+      <pointLight distance={26} castShadow shadow-bias={-0.0001} position={[-5, 4, 1]} />
       <Experience />
-    
+      <Effects />
     </Canvas>
   );
 }
