a3d7e0134ea04060cc0faa0cd4ead995da4e7662
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2023-03-16 13:34:32 -0600

    fixed the html iframe issue

--- src/components/WorkStation/WorkStation.jsx
+++ src/components/WorkStation/WorkStation.jsx
@@ -1,22 +1,22 @@
 import * as THREE from "three";
-import React,{useEffect} from "react";
+import React from "react";
 import { useState, useRef } from "react";
 import { Html, useGLTF } from "@react-three/drei";
-import { Bounds } from "@react-three/drei";
-import { MdOutlineOpenInNew } from "react-icons/md";
 import { useThree, useFrame } from "@react-three/fiber";
+import { MdOutlineOpenInNew } from "react-icons/md";
+import { Bounds } from "@react-three/drei";
 
-function CameraController({ targetRef, children }) {
+function CameraController({ targetRef, children, renderHtml, setHtmlVisible }) {
   const { camera } = useThree();
   const [focused, setFocused] = useState(false);
 
   useFrame(() => {
     if (focused && targetRef.current) {
-      const screenPosition = new THREE.Vector3(0.225, 0.95, -0.25);
+      const screenPosition = new THREE.Vector3(0.3, 0.95, 0); // Adjust laptop on focus
       targetRef.current.localToWorld(screenPosition);
-      const offset = new THREE.Vector3(0, 0, -1); // Adjust the offset values to set the camera distance from the screen
+      const offset = new THREE.Vector3(-0.2, 0.05, -1); // Adjust offset of laptop on focus
       const targetPosition = screenPosition.clone().add(offset);
-      camera.position.lerp(targetPosition, 0.020);
+      camera.position.lerp(targetPosition, 0.03); // Adjust speed of camera movement on focus
       camera.lookAt(screenPosition);
     }
   });
@@ -25,13 +25,76 @@ function CameraController({ targetRef, children }) {
     <group>
       {React.cloneElement(children, {
         ref: targetRef,
-        onClick: () => setFocused(!focused),
+        onClick: () => {
+          setFocused(!focused);
+          setHtmlVisible((prevHtmlVisible) => !prevHtmlVisible);
+        },
       })}
     </group>
   );
 }
 
 export function WorkStation() {
+  const workStationRef = useRef();
+  const [htmlVisible, setHtmlVisible] = useState(false);
+
+  const renderHtml = (visible) => {
+    if (visible) {
+      return (
+        <Html
+          transform
+          wrapperClass="browser"
+          distanceFactor={0.97}
+          position={[0.025, 0.85, -0.65]} // Adjust the position to be in front of the laptop
+          occlude={true}
+        >
+          <div className="browser-tab">
+            <button
+              onClick={() => handleTabClick(1)}
+              className={activeTab === 1 ? "active" : ""}
+            >
+              Project 1
+              <a target="_blank" href="https://danneytrieu.design/">
+                <MdOutlineOpenInNew />
+              </a>
+            </button>
+            <button
+              onClick={() => handleTabClick(2)}
+              className={activeTab === 2 ? "active" : ""}
+            >
+              Project 2
+              <a target="_blank" href="https://nam-cung.com/">
+                <MdOutlineOpenInNew />
+              </a>
+            </button>
+            <button
+              onClick={() => handleTabClick(3)}
+              className={activeTab === 3 ? "active" : ""}
+            >
+              Project 3
+              <a target="_blank" href="https://www.vrarlesfestival.com/">
+                <MdOutlineOpenInNew />
+              </a>
+            </button>
+            <button
+              onClick={() => handleTabClick(4)}
+              className={activeTab === 4 ? "active" : ""}
+            >
+              Project 4
+              <a target="_blank" href="https://bloquo.cc/">
+                <MdOutlineOpenInNew />
+              </a>
+            </button>
+          </div>
+          <iframe title={`project ${activeTab}`} src={getIframeSource()} />
+        </Html>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  //Model imports
   const tableModel = useGLTF(
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/table-wood/model.gltf"
   );
@@ -60,79 +123,27 @@ export function WorkStation() {
     }
   };
 
-  const laptopRef = useRef();
-  
-
   return (
-    // <Bounds fit clip observe margin={0.5}>
-    <CameraController targetRef={laptopRef}>
-      <primitive
-        position={[4.3, -0.7, 7.4]}
-        rotation={[0, -Math.PI + 0.6, 0]}
-        object={tableModel.scene}
-      >
+    <CameraController
+      targetRef={workStationRef}
+      setHtmlVisible={setHtmlVisible}
+      renderHtml={(focused) => renderHtml(focused)}
+    >
+      <group position={[1, -0.7, 2]} rotation={[0, -Math.PI + 0.8, 0]}>
+        <primitive object={tableModel.scene} />
         <primitive
           object={chairModel.scene}
-          position={[-0.3, 0, 1.2]}
+          position={[-0.2, 0, 1.2]}
           scale={"0.7"}
         />
         <primitive
           object={laptopModel.scene}
-          position={[0, 0.75, 0.2]}
+          position={[0, 0.75, 0.3]}
           scale={0.3}
         >
-          <Html
-            transform
-            wrapperClass="browser"
-            distanceFactor={0.97}
-            position={[0.025, 0.85, -0.65]}
-            rotation-x={0}
-            occlude={true}
-          >
-            <div className="browser-tab">
-              <button
-                onClick={() => handleTabClick(1)}
-                className={activeTab === 1 ? "active" : ""}
-              >
-                Project 1
-                <a target="_blank" href="https://danneytrieu.design/">
-                  <MdOutlineOpenInNew />
-                </a>
-              </button>
-              <button
-                onClick={() => handleTabClick(2)}
-                className={activeTab === 2 ? "active" : ""}
-              >
-                Project 2
-                <a target="_blank" href="https://nam-cung.com/">
-                  <MdOutlineOpenInNew />
-                </a>
-              </button>
-              <button
-                onClick={() => handleTabClick(3)}
-                className={activeTab === 3 ? "active" : ""}
-              >
-                Project 3
-                <a target="_blank" href="https://www.vrarlesfestival.com/">
-                  <MdOutlineOpenInNew />
-                </a>
-              </button>
-              <button
-                onClick={() => handleTabClick(4)}
-                className={activeTab === 4 ? "active" : ""}
-              >
-                Project 4
-                <a target="_blank" href="https://bloquo.cc/">
-                  <MdOutlineOpenInNew />
-                </a>
-              </button>
-            </div>
-            <iframe title={`project ${activeTab}`} src={getIframeSource()} />
-          </Html>
+          {renderHtml(htmlVisible)}
         </primitive>
-      </primitive>
+      </group>
     </CameraController>
-
-    // </Bounds>
   );
 }
