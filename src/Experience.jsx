78e69a0dadb8ff9e0a3c651fd5e66b6249bb0b09
Szymon Kucharski <speckope@gmail.com>
2023-03-20 13:42:13 +0100

    Fixed incorrect import in Experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { WorkStation } from './components/WorkStation/WorkStation';
 import { Model as Island } from './components/Island/Scene';
 import Animal from './components/Animal/Animal';
-import { CustomText3D } from './components/Text3D/CustomText3D';
+import { CustomText3D } from './components/CustomText3D/CustomText3D';
 
 export function Experience() {
   return (
