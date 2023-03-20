c27735d53a116537e302dd133838f30f0687b738
Szymon Kucharski <speckope@gmail.com>
2023-03-20 13:14:05 +0100

    Added 3DText, made it look like selected one, positioned it above the island

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,13 @@
-import React, { Suspense } from "react";
-import { WorkStation } from "./components/WorkStation/WorkStation";
-import { Model as Island } from "./components/Island/Scene";
-import Animal from "./components/Animal/Animal";
+import React, { Suspense } from 'react';
+import { WorkStation } from './components/WorkStation/WorkStation';
+import { Model as Island } from './components/Island/Scene';
+import Animal from './components/Animal/Animal';
+import { CustomText3D } from './components/Text3D/CustomText3D';
 
 export function Experience() {
   return (
     <Suspense fallback={null}>
+      <CustomText3D text='Portfolio' />
       <Island />
       <WorkStation />
       <Animal />
---
+++ src/components/Text3D/CustomText3D.jsx
@@ -0,0 +1,34 @@
+import { Text3D, useMatcapTexture } from '@react-three/drei';
+
+/** Displaying 3D text */
+export function CustomText3D({
+  /** text to be displayed of type string */
+  text = 'Enter text as param',
+  /** props to override any arguments to Text3D component */
+  ...props
+}) {
+  // matcapTexture from here: https://github.com/emmelleppi/matcaps
+  const [matcapTexture] = useMatcapTexture('C47004_F9D00C_EDAF04_E09704', 256);
+
+  return (
+    <>
+      <Text3D
+        font='./fonts/helvetiker_regular.typeface.json'
+        size={4}
+        height={1}
+        curveSegments={20}
+        bevelEnabled={true}
+        bevelThickness={0.15}
+        bevelSize={0.15}
+        bevelOffset={0}
+        bevelSegments={1}
+        letterSpacing={0.1}
+        position={[-5, 20, 2]}
+        {...props}
+      >
+        {text}
+        <meshMatcapMaterial matcap={matcapTexture} wireframes={true} />
+      </Text3D>
+    </>
+  );
+}
