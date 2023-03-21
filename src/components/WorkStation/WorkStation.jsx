cde2e5c14d60eb59eb1bd42d0ff1a09a2dc9c951
Szymon Kucharski <speckope@gmail.com>
2023-03-21 18:57:23 +0100

    Temporarily commented CameraController in WorkStation.jsx

--- src/components/WorkStation/WorkStation.jsx
+++ src/components/WorkStation/WorkStation.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useRef } from "react";
-import { Html, useGLTF } from "@react-three/drei";
-import { MdOutlineOpenInNew } from "react-icons/md";
-import { CameraController } from "./camera/CameraController";
+import React, { useState, useRef } from 'react';
+import { Html, useGLTF } from '@react-three/drei';
+import { MdOutlineOpenInNew } from 'react-icons/md';
+import { CameraController } from './camera/CameraController';
 
 export function WorkStation() {
   const workStationRef = useRef();
@@ -14,45 +14,45 @@ export function WorkStation() {
       return (
         <Html
           transform
-          wrapperClass="browser"
+          wrapperClass='browser'
           distanceFactor={0.97}
           position={[0.025, 0.85, -0.65]}
           occlude={true}
         >
-          <div className="browser-tab">
+          <div className='browser-tab'>
             <button
               onClick={() => handleTabClick(1)}
-              className={activeTab === 1 ? "active" : ""}
+              className={activeTab === 1 ? 'active' : ''}
             >
               Project 1
-              <a target="_blank" href="https://danneytrieu.design/">
+              <a target='_blank' href='https://danneytrieu.design/'>
                 <MdOutlineOpenInNew />
               </a>
             </button>
             <button
               onClick={() => handleTabClick(2)}
-              className={activeTab === 2 ? "active" : ""}
+              className={activeTab === 2 ? 'active' : ''}
             >
               Project 2
-              <a target="_blank" href="https://nam-cung.com/">
+              <a target='_blank' href='https://nam-cung.com/'>
                 <MdOutlineOpenInNew />
               </a>
             </button>
             <button
               onClick={() => handleTabClick(3)}
-              className={activeTab === 3 ? "active" : ""}
+              className={activeTab === 3 ? 'active' : ''}
             >
               Project 3
-              <a target="_blank" href="https://www.vrarlesfestival.com/">
+              <a target='_blank' href='https://www.vrarlesfestival.com/'>
                 <MdOutlineOpenInNew />
               </a>
             </button>
             <button
               onClick={() => handleTabClick(4)}
-              className={activeTab === 4 ? "active" : ""}
+              className={activeTab === 4 ? 'active' : ''}
             >
               Project 4
-              <a target="_blank" href="https://bloquo.cc/">
+              <a target='_blank' href='https://bloquo.cc/'>
                 <MdOutlineOpenInNew />
               </a>
             </button>
@@ -67,12 +67,12 @@ export function WorkStation() {
 
   // Import the 3D models for the table, chair, and laptop
   const tableModel = useGLTF(
-    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/table-wood/model.gltf"
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/table-wood/model.gltf'
   );
   const chairModel = useGLTF(
-    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bench-2/model.gltf"
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bench-2/model.gltf'
   );
-  const laptopModel = useGLTF("./models/laptop/laptop.glb");
+  const laptopModel = useGLTF('./models/laptop/laptop.glb');
 
   // Function to handle tab clicks and update the activeTab state
   const [activeTab, setActiveTab] = useState(1);
@@ -84,41 +84,41 @@ export function WorkStation() {
   const getIframeSource = () => {
     switch (activeTab) {
       case 1:
-        return "https://danneytrieu.design/";
+        return 'https://danneytrieu.design/';
       case 2:
-        return "https://nam-cung.com/";
+        return 'https://nam-cung.com/';
       case 3:
-        return "https://www.vrarlesfestival.com/";
+        return 'https://www.vrarlesfestival.com/';
       case 4:
-        return "https://bloquo.cc/";
+        return 'https://bloquo.cc/';
       default:
-        return "https://danneytrieu.design/";
+        return 'https://danneytrieu.design/';
     }
   };
 
   return (
     // Render the CameraController component with the 3D models and the Html element
-    <CameraController
-      targetRef={workStationRef}
-      focused={focused}
-      setFocused={setFocused}
-      setHtmlVisible={setHtmlVisible}
-    >
-      <group position={[1, -0.7, 2]} rotation={[0, -Math.PI + 0.8, 0]}>
-        <primitive object={tableModel.scene} />
-        <primitive
-          object={chairModel.scene}
-          position={[-0.2, 0, 1.2]}
-          scale={"0.7"}
-        />
-        <primitive
-          object={laptopModel.scene}
-          position={[0, 0.75, 0.3]}
-          scale={0.3}
-        >
-          {renderHtml(htmlVisible)}
-        </primitive>
-      </group>
-    </CameraController>
+    // <CameraController
+    //   targetRef={workStationRef}
+    //   focused={focused}
+    //   setFocused={setFocused}
+    //   setHtmlVisible={setHtmlVisible}
+    // >
+    <group position={[1, -0.7, 2]} rotation={[0, -Math.PI + 0.8, 0]}>
+      <primitive object={tableModel.scene} />
+      <primitive
+        object={chairModel.scene}
+        position={[-0.2, 0, 1.2]}
+        scale={'0.7'}
+      />
+      <primitive
+        object={laptopModel.scene}
+        position={[0, 0.75, 0.3]}
+        scale={0.3}
+      >
+        {renderHtml(htmlVisible)}
+      </primitive>
+    </group>
+    // </CameraController>
   );
 }
