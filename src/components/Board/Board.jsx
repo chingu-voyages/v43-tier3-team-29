5eba23e2a417f93e060ec6840effbfd3d11baba0
Szymon Kucharski <speckope@gmail.com>
2023-03-24 12:37:14 +0100

    Positioned iframe inside the front side of th board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,23 +1,27 @@
 import { Html, useGLTF } from '@react-three/drei';
-import BoardLink from './BoardLink';
 
 function Board(props) {
   const boardModel = useGLTF('/models/board/board.glb');
 
   return (
-    <>
-      <group position={[-6, 0.2, 4]} scale={7}>
-        <primitive {...props} object={boardModel.scene} />
-        <Html
-          center
-          as='div'
-          wrapperClass='board-links-container'
-          distanceFactor={1}
-        >
-          <BoardLink text='link no.1' href='google.com' />
-        </Html>
-      </group>
-    </>
+    <primitive object={boardModel.scene} position={[-6, 0.2, 4]} scale={7}>
+      <Html
+        transform
+        distanceFactor={0.1}
+        // rotation
+        rotation-x={0.19}
+        rotation-y={0.085}
+        // position
+        position-x={-0.002}
+        position-y={0.01}
+        position-z={-0.028}
+        wrapperClass='board-frame'
+        // Now it won't show through the board
+        occlude={true}
+      >
+        <iframe frameborder='0' />
+      </Html>
+    </primitive>
   );
 }
 
