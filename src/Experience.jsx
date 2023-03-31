b93282070e5933a168aa3731692c4aaecb191aa6
Jane Moroz <Moroz.jane@yahoo.com>
2023-03-31 18:15:06 +0300

    Add Character 2 (Sean)

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -65,11 +65,19 @@ export function Experience() {
       <Animal />
       {/* Character 1 / Danney */}
       <Character
-        path="./models/human/character_01_final-v1-transformed.glb"
+        path="./models/characters/character_01_final-v1-transformed.glb"
         scale={1.2}
         position={[-11, -1, 23]}
         rotation={[0, -0.3, 0]}
         actionName="Idle"
+      />
+      {/* Character 2 / Sean */}
+      <Character
+        path="./models/characters/character_02_final-v1-transformed.glb"
+        scale={1.2}
+        position={[-9.5, -0.62, 24.5]}
+        rotation={[1.6, 0, 1]}
+        actionName="Sitting"
       />
       <Lights />
       <Background />
