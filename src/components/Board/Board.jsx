25c7ad960bf5f58ebdc55e8bb0a57893fe1b7a15
Szymon Kucharski <speckope@gmail.com>
2023-04-14 20:57:33 +0200

    Positioned frog next to the board & chairs with idle animation

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,14 @@
-import { Html, useGLTF } from "@react-three/drei";
-import { editable as e } from "@theatre/r3f";
+import { Html, useGLTF } from '@react-three/drei';
+import { editable as e } from '@theatre/r3f';
 
 function Board(props) {
-  const boardModel = useGLTF("/models/board/board.glb");
+  const boardModel = useGLTF('/models/board/board.glb');
 
   return (
-    <e.group theatreKey="board">
+    <e.group theatreKey='board'>
       <primitive
         object={boardModel.scene}
-        position={[-12.7, -0.05, -7.2]}
+        position={[-12.5, -0.05, -8.2]}
         scale={7}
         rotation={[0, 0.95, 0]}
       >
@@ -22,11 +22,11 @@ function Board(props) {
           position-x={-0.002}
           position-y={0.01}
           position-z={-0.028}
-          wrapperClass="board-frame"
+          wrapperClass='board-frame'
           // Now it won't show through the board
           occlude={true}
         >
-          <iframe frameBorder="0" src="" />
+          <iframe frameBorder='0' src='' />
         </Html>
 
         <Html
@@ -40,11 +40,11 @@ function Board(props) {
           position-x={0.002}
           position-y={0.01}
           position-z={0.028}
-          wrapperClass="board-frame"
+          wrapperClass='board-frame'
           // Now it won't show through the board
           occlude={true}
         >
-          <iframe frameBorder="0" />
+          <iframe frameBorder='0' />
         </Html>
       </primitive>
     </e.group>
