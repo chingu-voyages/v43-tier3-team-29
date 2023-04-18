5be58c09b4095332f458f654eef3018ea234ceed
seantai <seantai@protonmail.com>
2023-04-18 15:34:30 -0700

    edit About Us words

--- src/components/Overlay/SectionDetails/sectionContentData.jsx
+++ src/components/Overlay/SectionDetails/sectionContentData.jsx
@@ -8,10 +8,10 @@ const sectionsContentData = [
     title: "About us",
     content: (
       <p>
-        The app was build during the wonderful Voyage which was organized by Jim
-        Medlock and his awesome team. Thanks to this opportunity we gained
-        confidence and levelled up soft and technical skills to face any future
-        challenges.
+        The app was built during the wonderful Voyage #43 organized by Jim
+        Medlock and his awesome Chingu team. Thanks to this opportunity we
+        gained confidence and levelled up soft and technical skills to face any
+        future challenges.
       </p>
     ),
   },
