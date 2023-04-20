c5678bafa2ec88e99e6b0280a20f538459f4d0f2
JaneMoroz <moroz.jane@yahoo.com>
2023-04-20 12:18:25 +0400

    Add credits

--- src/components/Overlay/SectionDetails/sectionContentData.jsx
+++ src/components/Overlay/SectionDetails/sectionContentData.jsx
@@ -16,7 +16,28 @@ const sectionsContentData = [
     ),
   },
   { key: "team", title: "Our team", content: <TeamCarousel /> },
-  { key: "credits", title: "Credits", content: <p>Credits</p> },
+  {
+    key: "credits",
+    title: "Credits",
+    content: (
+      <ul className="credits">
+        <li>
+          <a href="https://poly.pizza">Poly Pizza</a>
+        </li>
+        <li>
+          <a href="https://www.syntystudios.com">Synty Studios</a>
+        </li>
+        <li>
+          <a href="https://streakbyte.com">Streak Byte</a>
+        </li>
+        <li>
+          <a href="https://github.com/chingu-voyages/v43-tier3-team-29">
+            More Credits
+          </a>
+        </li>
+      </ul>
+    ),
+  },
 ];
 
 export default sectionsContentData;
