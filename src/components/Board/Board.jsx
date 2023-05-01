7f9cfbd33d0cea5e9811b84a0280dedc5b673086
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2023-05-01 14:19:54 -0600

    added board context

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,11 +1,11 @@
-import { Html, useGLTF } from '@react-three/drei';
-import { editable as e } from '@theatre/r3f';
+import { Html, useGLTF } from "@react-three/drei";
+import { editable as e } from "@theatre/r3f";
 
 function Board(props) {
-  const boardModel = useGLTF('/models/board/board.glb');
+  const boardModel = useGLTF("/models/board/board.glb");
 
   return (
-    <e.group theatreKey='board'>
+    <e.group theatreKey="board">
       <primitive
         object={boardModel.scene}
         position={[-12.5, -0.05, -8.2]}
@@ -15,36 +15,24 @@ function Board(props) {
         <Html
           transform
           distanceFactor={0.1}
-          // rotation
-          rotation-x={0.19}
-          rotation-y={0.085}
-          // position
           position-x={-0.002}
           position-y={0.01}
           position-z={-0.028}
-          wrapperClass='board-frame'
-          // Now it won't show through the board
-          occlude={true}
+          rotation-y={Math.PI}
         >
-          <iframe frameBorder='0' src='' />
-        </Html>
-
-        <Html
-          transform
-          distanceFactor={0.1}
-          // rotation
-          rotation-x={-0.19}
-          rotation-y={0.085}
-          rotation-z={0.025}
-          // position
-          position-x={0.002}
-          position-y={0.01}
-          position-z={0.028}
-          wrapperClass='board-frame'
-          // Now it won't show through the board
-          occlude={true}
-        >
-          <iframe frameBorder='0' />
+          <div className="board">
+            <p className="board-title">Tech Stack:</p>
+            <ul className="board-list">
+              <li>React</li>
+              <li>React-fiber</li>
+              <li>Fiber-drei</li>
+              <li>Threatre</li>
+              <li>Framer-motion</li>
+              <li>Zustand</li>
+              <li>Leva</li>
+              <li>Vite</li>
+            </ul>
+          </div>
         </Html>
       </primitive>
     </e.group>
