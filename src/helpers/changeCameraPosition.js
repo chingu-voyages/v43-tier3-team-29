1d22e5bac6f45c180743c62f91621bc60f10758c
Jane Moroz <Moroz.jane@yahoo.com>
2023-05-03 12:36:56 +0300

    Fix nav btn click animation sequence

--- src/helpers/changeCameraPosition.js
+++ src/helpers/changeCameraPosition.js
@@ -1,23 +1,10 @@
 import { cameraMovementSheet } from "../animation/theatre";
 
 const changeCameraPosition = (position) => {
-  if (cameraMovementSheet.sequence.position === 11) {
-    cameraMovementSheet.sequence.play({
-      range: [0, position],
-      rate: 0.3,
-    });
-  } else if (position < cameraMovementSheet.sequence.position) {
-    cameraMovementSheet.sequence.play({
-      range: [position, cameraMovementSheet.sequence.position],
-      rate: 0.3,
-      direction: "reverse",
-    });
-  } else {
-    cameraMovementSheet.sequence.play({
-      range: [cameraMovementSheet.sequence.position, position],
-      rate: 0.3,
-    });
-  }
+  cameraMovementSheet.sequence.play({
+    range: [position - 0.3, position],
+    rate: 0.3,
+  });
 };
 
 export default changeCameraPosition;
